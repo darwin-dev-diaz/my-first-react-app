7a2c97f17ce6285c76538e9cb34f9b17ee2bdabc
Darwin Diaz <152736334+darwin-dev-diaz@users.noreply.github.com>
2024-07-08 09:34:03 -0400

    Learn about keys

---
+++ src/LearningKeys.jsx
@@ -0,0 +1,15 @@
+const todos = [
+  { task: "mow the yard", id: uuid() },
+  { task: "Work on Odin Projects", id: uuid() },
+  { task: "feed the cat", id: uuid() },
+];
+
+function TodoList() {
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>{todo.task}</li>
+      ))}
+    </ul>
+  );
+}
