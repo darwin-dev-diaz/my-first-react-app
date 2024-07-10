fc16b8cda5dff545412a49f266939f3657cb92a1
Darwin Diaz <152736334+darwin-dev-diaz@users.noreply.github.com>
2024-07-10 11:29:24 -0400

    Old learning adfdas

--- src/PassingData.jsx
+++ src/PassingData.jsx
@@ -1,15 +1,38 @@
-function Button(){
-    return(
-        <button>Click Me!</button>
-    )
+function Button({
+  text = "This is a default prop",
+  color = "green",
+  fontSize = 12,
+  backgroundColor = "lightgrey",
+  handleClick,
+}) {
+  const buttonStyle = {
+    color: color,
+    fontSize: fontSize + "px",
+    backgroundColor: backgroundColor,
+  };
+
+  return (
+    <button onClick={()=>handleClick("https://www.theodinproject.com")} style={buttonStyle}>
+      {text}
+    </button>
+  );
 }
 
-export default function App(){
-    return(
-        <div>
-            <Button/>
-            <Button/>
-            <Button/>
-        </div>
-    )
+export default function App() {
+    const handleButtonClick = (url) => {
+        window.location.href = url;
+    }
+
+  return (
+    <div>
+      <Button handleClick={handleButtonClick}/>
+      <Button
+        text="Don't click Me!"
+        color="red"
+        fontSize={20}
+        backgroundColor="pink"
+      />
+      <Button handleClick={handleButtonClick}/>
+    </div>
+  );
 }
