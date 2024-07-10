38da8ae3b900074a7fded331a8df7abcff6049ff
Darwin Diaz <152736334+darwin-dev-diaz@users.noreply.github.com>
2024-07-10 11:47:49 -0400

    Learning state

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App.jsx'
-import './index.css'
+// import './index.css'
 // import Avatar from './Avatar.jsx'
 // import Todo from './Todo.jsx'
 // import AnimalApp from './AnimalApp.jsx'
 // import PackingList from './Condition.jsx'
 // import RenderLists from './RenderLists.jsx'
-import App from './PassingData.jsx'
+// import App from './PassingData.jsx'
+import Gallery from './LearningState'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Gallery />
   </React.StrictMode>,
 )
